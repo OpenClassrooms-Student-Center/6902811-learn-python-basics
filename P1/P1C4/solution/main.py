ef5b944fb6eb8927144d5af9106f60d47d648cbe
PierreBenoitLagoeyte <147721477+PierreBenoitLagoeyte@users.noreply.github.com>
2024-04-09 15:32:20 +0200

    main.py

--- P1/P1C4/solution/main.py
+++ P1/P1C4/solution/main.py
@@ -6,7 +6,7 @@ age = 25
 print(f"My name is {name} and I am {age} years old.")
 
 # Modifying the age variable
-age = age + 10  #ou "age += 10"
+age = age + 10  #or "age += 10"
 
 # Displaying the updated sentence with the variables
 print(f"My name is {name} and I am now {age} years old.")
